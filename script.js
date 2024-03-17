417f28eacf8bfab8aadfc066e7f8642ebf2eb40e
Aneathaa <159550441+AneathaaRse@users.noreply.github.com>
2024-03-17 23:14:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('survey-form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  // Perform validation here
+  // If validation passes, submit the form data
+});
